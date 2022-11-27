44892c2e5a88bf1cf8bdbba3aefdcbe75d4df90e
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-11-27 13:31:46 -0300

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { async } from "@firebase/util";
+import { useEffect, useState } from "react";
 import * as C from "./App.styles";
+import * as Photos from "./services/photos";
+import { Photo } from "./types/Photo";
+import { PhotoItem } from "./Components/PhotoItem";
+
 const App = () => {
-  return <C.Container>Jhow</C.Container>;
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    };
+
+    getPhotos();
+  }, []);
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        {loading && (
+          <C.ScreenWarning>
+            <div className="emoji">✋</div>
+            <div> Carregando....</div>
+          </C.ScreenWarning>
+        )}
+
+        {!loading && photos.length > 0 && (
+          <C.PhotoList>
+            {photos.map((item, index) => (
+              <PhotoItem key={index} url={item.url} name={item.name} />
+            ))}
+          </C.PhotoList>
+        )}
+
+        {!loading && photos.length === 0 && (
+          <C.ScreenWarning>
+            <div className="emoji">😞</div>
+            <div> Não há fotos cadastradas</div>
+          </C.ScreenWarning>
+        )}
+      </C.Area>
+    </C.Container>
+  );
 };
 
 export default App;
--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEAGINGSENDERID,
-    appId: process.env.REACT_APP_FIREBASE_APPID
-  };
+  apiKey: "AIzaSyD8GvCunJtsVnKtnz1fdE37Ltyz-5M8Z_g",
+  authDomain: "gallery-93da9.firebaseapp.com",
+  projectId: "gallery-93da9",
+  storageBucket: "gallery-93da9.appspot.com",
+  messagingSenderId: "268722590461",
+  appId: "1:268722590461:web:66c1070fa2bc4e58ca2494"
+};
   
   const firebaseApp = initializeApp(firebaseConfig);
   export const storage = getStorage(firebaseApp)
